43731b0fb02f39dab0a06167e74e6c6ccb2dc030
guojian21cn <guojian21cn@gmail.com>
2017-01-30 00:45:39 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,13 +9,11 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
     @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
-    
+
   end
 
   def edit
   end
-
-
 
   def new
     @group = Group.new
@@ -26,21 +24,23 @@ class GroupsController < ApplicationController
     @group.user = current_user
     if @group.save
 
+      current_user.join!(@group)
+
       redirect_to groups_path
     else
       render :new
-  end
-end
+    end
+   end
 
   def update
 
     if @group.update(group_params)
 
     redirect_to groups_path, notice: "Update Success"
-  else
+    else
     render :edit
+    end
   end
-end
 
   def destroy
 
@@ -49,6 +49,31 @@ end
     redirect_to groups_path
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功!"
+    else
+      flash[:warning] = "你已经是本讨论版成员了"
+    end
+
+    redirect_to group_path(@group)
+   end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版"
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+    end
+
+    redirect_to group_path(@group)
+   end
 
     private
 
@@ -64,6 +89,4 @@ end
     def group_params
       params.require(:group).permit(:title, :description)
     end
-
-
 end
